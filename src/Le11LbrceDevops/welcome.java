9602af2f13f6eb65a8c4db62efc47d681b1f9e07
AdmiN <AdmiN@ITWS000>
2024-08-21 14:56:35 +0530

    newone

---
+++ src/Le11LbrceDevops/welcome.java
@@ -0,0 +1,21 @@
+package Le11LbrceDevops;
+
+public class welcome {
+
+
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+
+		System.out.println("welcome");
+
+		
+
+
+
+	}
+
+
+
+}
